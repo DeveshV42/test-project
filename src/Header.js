d69273acca963018f76f683d0c1e3494e311b0a3
Devesh Vashistha <devesh.vashistha@inforcea.com>
2024-04-17 12:28:57 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ backgroundColor: '#f8f9fa', padding: '10px' }}>
+      <h1>My React Application</h1>
+    </header>
+  );
+}
+
+export default Header;
