a38a42519f3f0bd0794c791f0088ac64905e09c6
Devesh Vashistha <devesh.vashistha@inforcea.com>
2024-04-17 12:26:46 +0530

    commit

---
+++ src/ComponentOne.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ComponentOne() {
+  return (
+    <div style={{ padding: '10px', border: '1px solid black' }}>
+      <p>This is Component One</p>
+    </div>
+  );
+}
+
+export default ComponentOne;
---
+++ src/ComponentTwo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ComponentTwo() {
+  return (
+    <div style={{ padding: '10px', border: '1px solid black' }}>
+      <p>This is Component Two</p>
+    </div>
+  );
+}
+
+export default ComponentTwo;
